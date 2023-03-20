ae61bee32200890617d9b0c4101d1d8e6879cb3f
Kennedy <tmalonga737@gmail.com>
2023-03-20 04:38:21 +0200

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,4 +1,25 @@
-const {JSDOM} = require('jsdom')
+const { JSDOM } = require('jsdom')
+
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+        if(resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`)
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if(contentType.includes("text/html")) {
+            console.log(`non html response type: ${contentType}, on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    } catch (err) {
+        console.log(`error in fatch ${err.message} can't fatch ${currentURL}`)
+    }
+}
 
 //html body and links pushed to dom with JSON (absolute or relative URL)
 function getURLsFromHTML(htmlBody, baseURL) {
@@ -6,13 +27,13 @@ function getURLsFromHTML(htmlBody, baseURL) {
     const dom = new JSDOM(htmlBody)
     const linkElements = dom.window.document.querySelectorAll('a')
 
-    for(const linkElement of linkElements) {
-        if(linkElement.href.slice(0, 1) === '/') {
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
             //relative
             try {
                 const urlObj = new URL(`${baseURL}${linkElement.href}`)
                 urls.push(urlObj.href)
-            } catch(err) {
+            } catch (err) {
                 console.log(`error with relative urls: ${err.message}`)
             }
         } else {
@@ -20,7 +41,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
             try {
                 const urlObj = new URL(linkElement.href)
                 urls.push(urlObj.href)
-            } catch(err) {
+            } catch (err) {
                 console.log(`error with absolute urls: ${err.message}`)
             }
         }
@@ -31,8 +52,8 @@ function getURLsFromHTML(htmlBody, baseURL) {
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    
-    if(hostPath.length > 0 && hostPath.slice(-1) === '/') {
+
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
         return hostPath.slice(0, -1)
     }
     return hostPath
@@ -40,5 +61,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
