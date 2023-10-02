e206046569c1cc88c22164ad15e7a8e7949c3bff
Kennedy <tmalonga737@gmail.com>
2023-10-02 07:48:15 +0200

    getURLsFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl')
+const { normalizeURL, getURLsFromHTML } = require('./crawl')
 const { test, expect } = require('@jest/globals')
 
 //url tests❤️
@@ -27,5 +27,50 @@ test('normalizeURL strip http', () => {
     const input = 'https://underrated/courses'
     const actual = normalizeURL(input)
     const expected = 'underrated/courses'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://underrated/courses/">Courses</a>
+        </body>
+    </html>
+    `
+
+    const inputBaseURL = 'https://underrated/courses'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://underrated/courses/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/level">levels</a>
+        </body>
+    </html>
+    `
+
+    const inputBaseURL = 'https://underrated/courses'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://underrated/courses/level']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="Invalid">Invalid URL</a>
+        </body>
+    </html>
+    `
+
+    const inputBaseURL = 'https://underrated/courses'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
