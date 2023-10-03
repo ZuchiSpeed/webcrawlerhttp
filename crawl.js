610c5ec1ba7dca86fae60445d76bce39cc23449f
Kennedy <tmalonga737@gmail.com>
2023-10-03 05:55:05 +0200

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,17 +1,39 @@
-const { JSDOM } = require('jsdom') 
+const { JSDOM } = require('jsdom')
+
+async function crawlPage(currentURL) {
+    console.log(`Crawling ${currentURL}`)
+    try {
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399) {
+            console.log(`Error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get('content-type')
+        if (!contentType.includes('text/html')) {
+            console.log(`Not html content on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+}
 
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
 
     const linkElements = dom.window.document.querySelectorAll('a')
-    for(const linkElement of linkElements) {
-        if(linkElement.href.slice(0, 1) === '/') {
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
             //relative url
             try {
                 const urlObj = new URL(`${baseURL}${linkElement.href}`)
                 urls.push(urlObj.href)
-            } catch(err) {
+            } catch (err) {
                 console.log(`Error with relative urls: ${err.message}`)
             }
         } else {
@@ -19,7 +41,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
             try {
                 const urlObj = new URL(linkElement.href)
                 urls.push(urlObj.href)
-            } catch(err) {
+            } catch (err) {
                 console.log(`Error with absolute urls: ${err.message}`)
             }
         }
@@ -31,8 +53,8 @@ function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
 
-    
-    if(hostPath.length > 0 && hostPath.slice(-1) === '/') {
+
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
         return hostPath.slice(0, -1)
     }
     return hostPath
@@ -40,5 +62,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("hallow world")
+const { crawlPage } = require('./crawl')
+
+function main() {
+    if(process.argv.length < 3) {
+        console.log("no website provided")
+        process.exit(1)
+    }
+    
+    if(process.argv.length > 3) {
+        console.log("too many arguments")
+        process.exit(1)
+    }
+    
+    const baseURL = process.argv[2]
+
+    console.log(`started crawl of ${baseURL}`)
+    crawlPage(baseURL)
+}
+
+main()
